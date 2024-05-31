14009f19c58567e88a084defb71fc3c96d70af3a
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 15:09:21 +0000

    install pillow and add url static media

--- mynote/mynote/urls.py
+++ mynote/mynote/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('login/', views.login),
     path('posts/', include('posts.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
