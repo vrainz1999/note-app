e08b56d63c3327b6279c008c104010de1a8f32e4
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 18:54:48 +0000

    next to link

--- mynote/users/views.py
+++ mynote/users/views.py
@@ -11,13 +11,13 @@ def register_view(request):
             return redirect("posts:list")
     else:
         form = UserCreationForm()
+    
+    # Add Tailwind CSS to the template  
+    form.fields['username'].widget.attrs['class'] = 'text-md block px-3 py-2  rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none'
+    
+    form.fields['password1'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
 
-        # Add Tailwind CSS to the template  
-        form.fields['username'].widget.attrs['class'] = 'text-md block px-3 py-2  rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none'
-        
-        form.fields['password1'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
-
-        form.fields['password2'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
+    form.fields['password2'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
     return render(request, "users/register.html", { "form": form })
 
 
@@ -27,16 +27,19 @@ def login_view(request):
         if form.is_valid():
             # Do Login 
             login(request, form.get_user())
-            return redirect("posts:list")
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
     else:
         form = AuthenticationForm()
 
-        # Add tailwindcss to the template
-        form.fields['username'].widget.attrs['class'] = 'text-md block px-3 py-2  rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none'
-        
-        form.fields['password'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
+    # Add tailwindcss to the template
+    form.fields['username'].widget.attrs['class'] = 'text-md block px-3 py-2  rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none'
+    
+    form.fields['password'].widget.attrs['class'] = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white  focus:border-gray-600 focus:outline-none'
 
-        # form.fields['error_messages'].widget.attrs['class'] = 'relative block w-full p-4 mb-4 text-base leading-5 text-white bg-red-500 rounded-lg opacity-100 font-regular' 
+    # form.fields['error_messages'].widget.attrs['class'] = 'relative block w-full p-4 mb-4 text-base leading-5 text-white bg-red-500 rounded-lg opacity-100 font-regular' 
     return render(request, "users/login.html", { "form": form })
 
 def logout_view(request):
