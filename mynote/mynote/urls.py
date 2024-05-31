f3b2827b38a20b321a1a4d4e7b35f2b2fa770eff
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 15:45:02 +0000

    add register page

--- mynote/mynote/urls.py
+++ mynote/mynote/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('login/', views.login),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
