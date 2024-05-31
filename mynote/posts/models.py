da0e5285d7a6007471ccf2e3ef2ee2c0f07cbb56
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 14:46:51 +0000

    make post detail, add slug model

--- mynote/posts/models.py
+++ mynote/posts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    slug = models.SlugField(unique=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- mynote/posts/views.py
+++ mynote/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('created_at')
     return render(request, 'posts/posts_list.html', { 'posts': posts })
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_detail.html', { 'post': post })
+
