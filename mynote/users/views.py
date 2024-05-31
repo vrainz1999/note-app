58bdf38e941f024aedc133111e9d144add85e3e2
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 18:38:10 +0000

    Add logout button in header and add logout func

--- mynote/users/views.py
+++ mynote/users/views.py
@@ -38,3 +38,8 @@ def login_view(request):
 
         # form.fields['error_messages'].widget.attrs['class'] = 'relative block w-full p-4 mb-4 text-base leading-5 text-white bg-red-500 rounded-lg opacity-100 font-regular' 
     return render(request, "users/login.html", { "form": form })
+
+def logout_view(request):
+    if request.method == "POST": 
+        logout(request) 
+        return redirect("users:login")
