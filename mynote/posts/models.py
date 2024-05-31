bd71cbc7a84ac54f8f720e00d00bf37d885a80eb
rama.4ditya3 <rama.4ditya3@gmail.com>
2024-05-31 15:19:47 +0000

    update model add some image

--- mynote/posts/models.py
+++ mynote/posts/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     slug = models.SlugField(unique=True, default=None)
+    image = models.ImageField(upload_to='images/', default='no_image.jpg')
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
